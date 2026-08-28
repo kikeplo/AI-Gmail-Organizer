54f74b65ddf615df9110a26d0b38e1581752ef0d
kikeplo <kristaps.pluto.official@gmail.com>
2026-08-29 01:23:04 +0300

    Add browser package

---
+++ app/browser/__init__.py
@@ -0,0 +1,4 @@
+"""Browser automation package."""
+from app.browser.playwright_controller import BrowserAutomationError, BrowserResult, PlaywrightController
+
+__all__ = ["BrowserAutomationError", "BrowserResult", "PlaywrightController"]
